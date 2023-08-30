48ef2e767b782b9a5992261d1b8ab22270b33116
Kamrul Touhid <kamrultouhids@gmail.com>
2023-08-30 18:15:17 +0600

    photoroom intgrate

--- src/PituraDefault.js
+++ src/PituraDefault.js
@@ -9,17 +9,72 @@ import {getEditorDefaults} from '@pqina/pintura';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
-
+import Col from "react-bootstrap/Col";
+const CLIP_API_REMOVE_BG = `https://sdk.photoroom.com/v1/segment`
+const CLIP_API_REPLACE_BG = `https://beta-sdk.photoroom.com/v1/render`
+const API_KEY = "27e9428e688f503335e699d229836d83e62f0e13"
 
 // get default properties
 const editorDefaults = getEditorDefaults({
-    stickers: [''],
-    utils: ['sticker', 'finetune', 'filter', 'annotate', 'resize'],
+  // utils: ['sticker', 'finetune', 'filter', 'annotate', 'resize'],
+  utils: ['sticker', 'annotate', 'crop', 'filter', 'resize'],
+  util: 'sticker'
+  // utils: ['annotate', 'finetune', 'filter', 'sticker', 'resize'],
 });
 
 // console.log("Editor Defaults",editorDefaults);
 export default function Example() {
-    const [result, setResult] = useState('');
+  const [loading, setLoading] = useState();
+
+  const [file, setFile] = useState('');
+  const [uploadFile, setUploadFile] = useState('https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Olive_wreath.svg/1259px-Olive_wreath.svg.png');
+
+  const handleFileChange = (e) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+      const makeImage =  URL.createObjectURL(e.target.files[0])
+      setUploadFile(makeImage)
+    }
+  };
+
+  const removeBackground = async() => {
+    if (!file) return;
+    setLoading(true);
+    let response
+
+    const fd = new FormData()
+    fd.append('image_file', file)
+
+
+    console.log("fd", fd)
+    const headers = {
+      'x-api-key': API_KEY
+    }
+
+    response = await fetch(CLIP_API_REMOVE_BG, {
+      method: 'POST',
+      headers,
+      body: fd,
+    })
+
+    await responseFormat(response);
+
+  };
+
+  const responseFormat = async(response) => {
+    if (response.status && response.status > 400) {
+      const text = await response.text()
+      throw new Error(response.status + ' ' + text)
+    }
+
+    const blob = await response.blob()
+    const base64 = await URL.createObjectURL(blob)
+    setUploadFile(base64)
+    setFile(blob);
+    setLoading(false);
+  }
+
+  const [result, setResult] = useState('');
     const [template, setTemplate] = useState('');
     const templates = [
         { name: 'Square, white background, 5% padding', image: 'template/door.jpg', templateId: '9124f52a-3512-48b7-80f1-2cb07a3607fb'},
@@ -29,7 +84,6 @@ export default function Example() {
     const handleChangeTemplate = (templateId) => {
         setTemplate(templateId)
     }
-    console.log(template);
 
     return (
         <div>
@@ -43,15 +97,38 @@ export default function Example() {
                     )}
                 </div>
             </Container>
+          <Container>
+            <Row>
+              <Col xs={3} md={4}>
+                <input style={{marginTop: '10px'}} type="file" onChange={handleFileChange} />
+              </Col>
+              <Col md={2}>
+                <button onClick={() => removeBackground ()}><b>Remove BG</b> </button>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                { loading && <div style={{marginTop: '10px'}} className="loader"><b>Loading.........</b></div> }
+              </Col>
+            </Row>
+          </Container>
 
                 <div style={{ height: '70vh' }}>
                     <PinturaEditor
                         {...editorDefaults}
                         src={ template}
-                        imageCropAspectRatio={1}
                         onLoad={(res) => console.log('load image', res)}
                         onProcess={({ dest }) => setResult(URL.createObjectURL(dest))}
-                        locale={{ stickerLabel: 'Upload' }}
+                        onSelectutil={(details)=>console.log('details...',details)}
+                        stickers={[
+                          {
+                            // src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Olive_wreath.svg/1259px-Olive_wreath.svg.png',
+                            src: uploadFile,
+                            width: 200,
+                            height: 200,
+                            alt: 'Number one',
+                          }
+                        ]}
                     />
                 </div>
 
